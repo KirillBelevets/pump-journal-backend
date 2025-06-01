16bf7c22e3e0b17562f8f4c6a69d9a8a454c1756
Kirill Belevets <kirill.belevets@gmail.com>
2025-06-01 21:56:43 +0300

    replace origin links

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      'http://localhost:3001',
       'https://pump-journal-frontend.vercel.app',
+      'http://localhost:3001',
     ],
     credentials: true,
   });

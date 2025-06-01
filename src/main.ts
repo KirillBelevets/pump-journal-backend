4ca56bd59ef85ec9fd05a6194054dac21b1f8e2a
Kirill Belevets <kirill.belevets@gmail.com>
2025-06-01 21:34:24 +0300

    add main origin frontend to enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: [
+      'http://localhost:3001',
+      'https://pump-journal-frontend.vercel.app',
+    ],
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
